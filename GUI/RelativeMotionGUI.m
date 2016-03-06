18360d73925b9f65caed637b5247cde4d4ae1e58
arog <arog3632@vt.edu>
2016-03-06 15:07:26 -0500

    Starting to work on a Matlab GUI to do relative motion simulations

---
+++ GUI/RelativeMotionGUI.m
@@ -0,0 +1,11 @@
+function RelativeMotionGUI
+
+clear all; close all; clc;
+
+f = figure('units','normalized','outerposition',[0 0 1 1],'name','Relative Motion');
+fHeight = f.Position(4);
+fWidth  = f.Position(3);
+
+
+
+end
