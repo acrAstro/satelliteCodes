b03eff34866e63b7af0a43d20671f006b8b35240
arog <arog3632@vt.edu>
2016-03-25 16:10:34 -0400

    Moved all auxiliary files to auxiliary folder

--- GUI/RelativeMotionGUI.m
+++ GUI/RelativeMotionGUI.m
@@ -1,7 +1,7 @@
 function RelativeMotionGUI
 
 clear; close all; clc;
-
+addpath([pwd,filesep,'Auxiliary Functions']);
 guiHeight = 500;
 guiWidth = 1000;
 leftBuffer = 50;
@@ -10,7 +10,7 @@ screenSize = get(groot,'Screensize');
 screenHeight = screenSize(4);
 f = figure('Visible','off','position',[leftBuffer,screenHeight-guiHeight-topBuffer,guiWidth,guiHeight]);
 
-
+chiefPanel = makePanel(f,'Chief Orbit',0.01,0.01,0.3,0.3);
 
 f.Visible = 'on';
 
