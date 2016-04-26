7aff265910064414fcfe0041d6e12e2e71c42bb7
arog <arog3632@vt.edu>
2016-04-26 07:09:21 -0400

    Wrote a few little C++ programs

---
+++ sat++/Orbit_Main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+  double t0 = 0;
+  double tf = 5400;
+  double N = 200;
+
+  TwoBodyOrbit tbp = {t0, tf, N};
+  return 0;
+
+}
--- sat++/TwoBodyOrbit.cc
+++ sat++/TwoBodyOrbit.cc
@@ -1,11 +1,42 @@
 #include <iostream>
 #include <math.h>
 
+/* This is a class that does basic orbital analysis based on the Kepler elements
+ * The primary functions are contained in the class, and the class is called
+ * from a different file at compilation
+*/
 class TwoBodyOrbit
 {
 public:
-  double J2;
-  double initialKeplerElems[6][1];
-  double safetyAltitude;
-  double time;
+  //double initialKeplerElems[6];
+  //double period;
+  //double meanMotion;
+  double initTime;
+  double finalTime;
+  int numSteps;
+  //double *time;
+  void printTime()
+  {
+    using namespace std;
+    cout  << "\n";
+    cout << initTime << ":" << numSteps << ":" << finalTime << endl;
+    cout << "\n";
+  }
+/*  double makeTimeVector()
+  {
+    time = new double[numSteps];
+    return time;
+  }*/
 };
+
+int main()
+{
+  double t0 = 0;
+  double tf = 5400;
+  double N = 200;
+
+  TwoBodyOrbit tbp = {t0, tf, N};
+  tbp.printTime();
+  return 0;
+
+}
