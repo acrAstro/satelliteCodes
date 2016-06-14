62d586a3935cb856ec7781580c5b6516a2c98583
arog <arog3632@vt.edu>
2016-06-13 23:25:50 -0400

    Catching up

--- sat++/circularOrbit/lcvProto.h
+++
@@ -1,9 +0,0 @@
-#ifndef LCVPROTO_H
-#define LCVPROTO_H
-
-// This header contains the function prototypes for the leoSpeed.cc function.
-double getSMA();
-double getCircularSpeed(double);
-double kpsToMPH(double);
-
-#endif
